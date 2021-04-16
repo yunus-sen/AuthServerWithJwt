eaaabd6ca14a70ec9126932a0435445ea9367db6
yunus-sen <yunussen2727@hotmail.com>
2021-04-16 18:06:05 +0300

    ErrorDto ve ResponseDto oluşturuldu.

---
+++ AuthServerWithJwt.Core/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace AuthServerWithJwt.Core
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsyn(int Id);
+        Task<IQueryable<TEntity>> GetAll();
+        Task Add(TEntity entity);
+        IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate);
+        void Remove(TEntity entity);
+        TEntity Update(TEntity entity);
+
+    }
+}
---
+++ AuthServerWithJwt.Shared/Dtos/ErrorDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerWithJwt.Shared.Dtos
+{
+    public class ErrorDto
+    {
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+        public ErrorDto(string error,bool isShow)
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = Errors;
+            IsShow = isShow;
+        }
+
+
+        public List<string> Errors{ get; private set; }
+        public bool IsShow { get; private set; }
+    }
+}
