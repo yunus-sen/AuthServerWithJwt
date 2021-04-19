104fa8324fbe78c7c506bf84654f6f097e6fa122
yunus-sen <yunussen2727@hotmail.com>
2021-04-19 12:27:03 +0300

    Generic service interface hazırlandı.

---
+++ AuthServerWithJwt.Core/Services/IGenericService.cs
@@ -0,0 +1,20 @@
+﻿using AuthServerWithJwt.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerWithJwt.Core.Services
+{
+    public interface IGenericService<TEntity, TDto> where TEntity : class where TDto : class
+    {
+        Task<Response<TDto>> GetByIdAsyn(int Id);
+        Task<Response<IQueryable<TDto>>> GetAll();
+        Task<Response<TDto>> Add(TEntity entity);
+        Response<IEnumerable<TDto>> Get(Expression<Func<TEntity, bool>> predicate);
+        Task<Response<NoDataDto>> Remove(TEntity entity);
+        Task<Response<TDto>> Update(TEntity entity);
+    }
+}
